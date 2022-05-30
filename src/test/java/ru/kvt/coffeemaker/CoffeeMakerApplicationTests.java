75a03d650ffcc95059a6df5bf81527dc6315f86d
KVT <milmilminmil@gmail.com>
2022-05-30 12:50:26 +0300

    tests

---
+++ src/test/java/ru/kvt/coffeemaker/CoffeeMakerApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.kvt.coffeemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CoffeeMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
